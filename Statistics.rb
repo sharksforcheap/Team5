585d93f90e22dd91d922d66b6fc7b8392d99ebed
Mehul Kar <mehul.kar@gmail.com>
2012-02-23 15:34:39 -0800

    completed first iteration of DisplayStatistics class

--- Statistics.rb
+++ Statistics.rb
@@ -1,3 +1,5 @@
+require './RubyMethods.rb'
+
 class Statistics
   def initialize(hash)
     @hash = hash
@@ -14,3 +16,31 @@ class Statistics
    @output_hash
   end
 end
+
+class DisplayStatistics
+  def initialize(statistics_hash)
+    @ruby_methods = RubyMethods.new("http://ruby-doc.org/core-1.9.3/").method_hash
+    @statistics_hash = statistics_hash
+    @output = ""
+  end
+  def format_and_display
+    @statistics_hash.keys.each do |method|
+      method_class_string = ""
+      @ruby_methods[method].each do |class_method|
+          method_class_string << class_method + "   "
+      end
+      @output << "#{method} (#{@statistics_hash[method]}): #{method_class_string} \n"
+    end
+    @output
+  end
+end
+
+#### Following code is for testing purposes, to retrieve the top 5 used methods and output their statsitics in a neat display.
+
+# @methods_hash = {"sort"=>3, "each"=>2, "find"=>1, 
+#   "split"=>43, "reverse"=>9, "pop"=>5, "center" => 12,
+#   "gsub" => 40 }
+# @stats = Statistics.new(@methods_hash)
+# @stats = @stats.top_list(5)
+# display = DisplayStatistics.new(@stats)
+# display.format_and_display
