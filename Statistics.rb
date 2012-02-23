6224f5757aac0b52c54d77a440dfd353100d0603
Mehul Kar <mehul.kar@gmail.com>
2012-02-23 12:47:08 -0800

    added additional statistics and their respective tests

--- Statistics.rb
+++ Statistics.rb
@@ -4,13 +4,13 @@ class Statistics
   end
   
   def top_five
-    @output_hash = {}
-    top_five_list = @hash.sort_by {|key, value| value}.reverse[0..4]
-    top_five_list.each { |pair| @output_hash[pair[0]] = pair[1] }
-    @output_hash
+    top_list(5)
   end
   
-  def outliers
-    
+  def top_list(integer)
+   @output_hash = {}
+   top_x_list = @hash.sort_by {|key, value| value}.reverse[0..(integer-1)]
+   top_x_list.each { |pair| @output_hash[pair[0]] = pair[1] }
+   @output_hash
   end
 end
--- statistics_spec.rb
+++ statistics_spec.rb
@@ -1,4 +1,4 @@
-require "./statistics.rb"
+require "./Statistics.rb"
 
 describe "Statistics" do
   before :each do
@@ -15,9 +15,8 @@ describe "Statistics" do
   it "should return a top 5 list" do
     @stats.top_five.should == {"really_popular" => 80, "split"=>43, "gsub"=>40, "christian"=>30, "lachy"=>17}
   end
-  it "should return the outliers" do
-    @stats.outliers.should == { A LIST OF OUTLIERS}
+  it "should return the top (user input) list" do
+    @stats.top_list(5).should == {"really_popular" => 80, "split"=>43, "gsub"=>40, "christian"=>30, "lachy"=>17}
+    @stats.top_list(10).should == {"really_popular" => 80, "split"=>43, "gsub"=>40, "christian"=>30, "lachy"=>17, "tony"=>16, "center"=> 12, "reverse"=> 9, "pop"=> 5, "sort"=>3}
   end
-  it "should return the bottom 5 list"
-  it "should return the median"
-end
+ end
