d3920dac28a7752b5e9ef79a50606d624f6477d9
Lachy Groom <lachygroom+collectivejam@gmail.com>
2012-02-23 13:13:23 -0800

    moved some of the testing files into testing folder and removed the others from the repo

--- test_commenting.rb
+++
@@ -1,5 +0,0 @@
-def hello
-  'tony'.my_special_method.reverse.each.split
-  dog_list.each.find #tony.reverse.each.split.etc.test.reverse
-  #test.each
-end
--- test_interpolation.rb
+++
@@ -1,5 +0,0 @@
-def hello
-  'tony'.my_special_method.reverse.each.split
-  dog_list.each.find
-  "this is an interpolation string #{dog_list.each.find.reverse}"
-end
