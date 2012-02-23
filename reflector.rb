e4814e8f2c99773c58ebb8749bdacfd34d9b50fb
Mehul Kar <mehul.kar@gmail.com>
2012-02-22 17:11:20 -0800

    refactors #top_five

--- reflector.rb
+++ reflector.rb
@@ -41,9 +41,7 @@ class Statistics
   def top_five
     @output_hash = {}
     top_five_list = @hash.sort_by {|key, value| value}.reverse[0..4]
-    top_five_list.each do |pair|
-      @output_hash[pair[0]] = pair[1]
-    end
+    top_five_list.each { |pair| @output_hash[pair[0]] = pair[1] }
     @output_hash
   end
 end
