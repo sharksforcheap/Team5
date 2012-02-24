8de47d27aa41b067dca78d4619685451c2060bfa
Lachy Groom <lachygroom+collectivejam@gmail.com>
2012-02-23 16:17:01 -0800

    added a string passing functionality to SourceCode initializer - roger and tony

--- reflector_rspec.rb
+++ reflector_rspec.rb
@@ -9,22 +9,31 @@ describe "SourceCode"  do
     end
     
     describe "#count_methods" do
+      before :each do
+        @input_hash = {'reverse'=>['string','hash','array'], 'split'=>['string'], 'each'=>['array', 'hash'], 'find'=>['array'], 'sort_by'=>['string', 'object']}
+      end
       it "should return an array of methods" do
-        input_hash = {'reverse'=>['string','hash','array'], 'split'=>['string'], 'each'=>['array', 'hash'], 'find'=>['array']}
-        @doc.count_methods(input_hash).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
+        @doc.count_methods(@input_hash).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
 
       it "should return methods followed by paranthesis as part of the #count_methods array" do
         @doc = SourceCode.new('tests/test_parens.rb')
-        input_hash = {'reverse'=>['string','hash','array'], 'split'=>['string'], 'each'=>['array', 'hash'], 'find'=>['array'], 'sort_by'=>['string', 'object']}
-        @doc.count_methods(input_hash).should == {"sort_by"=>1, "each"=>2, "split"=>2}
+        @doc.count_methods(@input_hash).should == {"sort_by"=>1, "each"=>2, "split"=>2}
       end
 
       it "should return an array of methods, ignoring anything commented." do
         @doc = SourceCode.new('tests/test_commenting.rb')
-        input_hash = {'reverse'=>['string','hash','array'], 'split'=>['string'], 'each'=>['array', 'hash'], 'find'=>['array']}
-        @doc.count_methods(input_hash).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
+        @doc.count_methods(@input_hash).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
+      
+      it "should return a method popularity hash when passing string" do
+        file_contents = "def hello
+          'tony'.my_special_method.reverse.each.split
+          dog_list.each.find
+        end"
+        SourceCode.new(file_contents, :string).count_methods(@input_hash).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
+      end
+      
     end
     
     describe "directory structure" do
